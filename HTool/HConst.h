513f17cf47f2355955731ce575ede550f18331f2
hare27 <hare27@foxmail.com>
2016-06-13 15:21:20 +0800

    1.4.4 修改了HConst的使用范围

--- HTool/HConst.h
+++ HTool/HConst.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+/** 只在oc的文件中，才能起作用*/
+#ifdef __OBJC__
+
+
+
 /** 自定义log*/
 #ifdef DEBUG
 #define HLog(...) NSLog(@"%s %s %d \n %@\n\n",__FILE__,__func__,__LINE__,[NSString stringWithFormat:__VA_ARGS__])
@@ -16,13 +21,19 @@
 #endif
 
 /** rgb颜色*/
-#define HColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+#define HRGBColor(r, g, b) HRGBAColor(r, g, b, 255)
+#define HRGBAColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)/255.0]
 
 /** 随机色*/
-#define HRandColor HColor(arc4random()%256, arc4random()%256, arc4random()%256)
+#define HRandColor HRGBAColor(arc4random()%256, arc4random()%256, arc4random()%256, arc4random()%256)
 
 /** 屏幕宽*/
 #define HScreenW [UIScreen mainScreen].bounds.size.width
 
 /** 屏幕高*/
 #define HScreenH [UIScreen mainScreen].bounds.size.height
+
+
+
+
+#endif
